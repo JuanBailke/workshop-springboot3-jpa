101ca6e28fe2b6d0057fd4bfbd963dfad60408e8
Juan Bailke <juan_bailke@hotmail.com>
2024-10-16 16:25:59 -0300

    Project created

---
+++ src/test/java/com/workshop/webservice/WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
